59649a2a77e5571ed33e0304ff6f71f89fdd67ff
Jose Gonzalez <joseojedapro@gmail.com>
2022-11-07 14:08:07 -0500

    updating manifest

--- filing_supplier_invoices/__manifest__.py
+++ filing_supplier_invoices/__manifest__.py
@@ -1,18 +1,18 @@
+# Copyright 2020 TECNOSOFTWARE S.A.S (<http://www.tecnosoftware.co>)
 # -*- coding: utf-8 -*-
 {
-    'name': "Filing Supplier Invoices",
-    'summary': """
-       """,
-    'author': "Jose Gonzalez / Tecnosoftware S.A.S",
-    'website': "https://www.tecnosoftware.co",
+    "name": "Filing Supplier Invoices",
+    "summary": """Add a invoice support to invoices & clasificate a purchase orders by the status of settling 
+    """,
+    "version": "15.0.1.0.0",
+    "author": "Jose Gonzalez / Tecnosoftware S.A.S",
+    "website": "https://www.tecnosoftware.co",
     "maintainers": ["tecnosoftcol"],
-    'category': "Warehouse",
     "license": "OPL-1",
-    'version': "14.0.1.0.0",
     "application": False,
     "installable": True,
-    'depends': ['purchase', 'account'],
-    'data': [
+    "depends": ['base', 'purchase', 'account'],
+    "data": [
         'security/filing_supplier_invoices_security.xml',
         'security/ir.model.access.csv',
         'views/purchase_order_views.xml',
@@ -21,7 +21,7 @@
         'views/documents_menuitems.xml',
         'data/ir_sequence_data.xml',
     ],
-    'demo': [
+    "demo": [
         'demo/documents_demo.xml'
     ]
 
