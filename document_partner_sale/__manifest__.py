03c754e6de0b87ff5963537dcc718d455dba0c08
Jose Gonzalez <joseojedapro@gmail.com>
2022-11-07 13:50:57 -0500

    updating manifest

--- document_partner_sale/__manifest__.py
+++ document_partner_sale/__manifest__.py
@@ -1,18 +1,17 @@
+# Copyright 2020 TECNOSOFTWARE S.A.S (<http://www.tecnosoftware.co>)
 # -*- coding: utf-8 -*-
-
 {
-    'name': 'Document Partner Sale',
-    'version': '1.0',
-    'description': '',
-    'summary': '',
-    'author': '',
-    'website': '',
-    'license': 'LGPL-3',
-    'category': '',
-    'depends': [
-        'sale'
-    ],
-    'data': [
+    "name": "Document Partner Sale",
+    "summary": """AAdd identification number(vat) and street of partner in tree view of sale order""",
+    "version": "15.0.1.0.0",
+    "author": "Jose Gonzalez / Tecnosoftware S.A.S",
+    "website": "https://www.tecnosoftware.co",
+    "maintainers": ["tecnosoftcol"],
+    "license": "OPL-1",
+    "application": False,
+    "installable": True,
+    "depends": ["base", "sale"],
+    "data": [
         'views/document_partner_view.xml'
     ],
 }
