64f7e83805e8f8c1a5a91fb4ea0240276e030e58
Jose Gonzalez <joseojedapro@gmail.com>
2022-11-09 13:59:38 -0500

    fixing manifest

--- neighborhood_partner_address/__manifest__.py
+++ neighborhood_partner_address/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Agrega el Barrio y su codigo postal asociado en contactos',
     'summary': """Agrega los campos los campos de barrio el codigo postal asociado en la informacion del contacto """,
-    'version': '12.0.0.1',
+    'version': '15.0.1.0.0',
     'category': 'Extra Tools',
     'author': 'Alcides Vega [TECNOSOFTWARE S.A.S.]',
     'website': 'http://www.tecnosoftware.co',
